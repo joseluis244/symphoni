35953e3965c6f1415b17f7dbf10bae1f9ee5e6e2
Jose Luis Camacho <jose.luis244@gmail.com>
2020-06-22 20:16:58 -0400

    asd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>nombre usuario</h1>
+        <div className="ListaAcciones">
+          <button className="Btn">crear usuario</button>
+          <button className="Btn">eliminar usuario</button>
+          <button className="Btn">modificar usuario</button>
+          <button className="Btn">crear cliente</button>
+          <button className="Btn">eliminar cliente</button>
+          <button className="Btn">modificar clientes</button>
+        </div>
+      </div>
     </div>
   );
 }
